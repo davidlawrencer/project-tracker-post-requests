3706d6e4df504512c72dfed2a9244a99035b304e
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-12 08:39:22 -0400

    Adds network helper and apperrors

---
+++ project-tracker-post-requests/AppError.swift
@@ -0,0 +1,21 @@
+//
+//  AppError.swift
+//  project-tracker-post-requests
+//
+//  Created by David Rifkin on 9/12/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case unauthenticated
+    case invalidJSONResponse
+    case couldNotParseJSON(rawError: Error)
+    case noInternetConnection
+    case badURL
+    case badStatusCode
+    case noDataReceived
+    case notAnImage
+    case other(rawError: Error)
+}
