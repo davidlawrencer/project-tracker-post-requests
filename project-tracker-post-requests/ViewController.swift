01ca7bc887b093feef1a420faa962bfbe4f6ff14
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-12 08:47:49 -0400

    Sets up tableView

--- project-tracker-post-requests/ViewController.swift
+++ project-tracker-post-requests/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProjectsViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Private Properties
+    
+    private var projects = [Project]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - ViewController Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+        loadData()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func loadData() {
+        ProjectAPIClient.manager.getProjects { result in
+            DispatchQueue.main.async { [weak self] in
+                switch result {
+                case let .success(projects):
+                    self?.projects = projects
+                case let .failure(error):
+                    self?.displayErrorAlert(with: error)
+                }
+            }
+        }
+    }
+    
+    private func displayErrorAlert(with error: AppError) {
+        let alertVC = UIAlertController(title: "Error Fetching Data", message: "\(error)", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
     }
 }
 
+
+extension ProjectsViewController: UITableViewDelegate {}
+
+extension ProjectsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let project = projects[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "projectCell", for: indexPath)
+        cell.textLabel?.text = project.name
+        cell.detailTextLabel?.text = project.dueDate.description
+        return cell
+    }
+}
