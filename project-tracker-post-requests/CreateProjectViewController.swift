3fbb3dba56ec63097ab4be7466b1975a9d4c5082
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-12 13:21:20 -0400

    Sets up create project in view controller

--- project-tracker-post-requests/CreateProjectViewController.swift
+++ project-tracker-post-requests/CreateProjectViewController.swift
@@ -17,11 +17,37 @@ class CreateProjectViewController: UIViewController {
     
     //MARK: IBAction
     @IBAction func createButtonPressed(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
+        
+        guard let project = createProjectFromFields() else {
+            //TODO: error... maybe a pop-up
+            return
+        }
+        
+        ProjectAPIClient.manager.postProject(project: project) { (result) in
+            switch result {
+            case .success(let success):
+            self.navigationController?.popViewController(animated: true)
+                //we could do something with the data
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     //MARK: Lifecycle Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //MARK: Private
+    private func createProjectFromFields() -> Project? {
+        guard let name = nameTextField.text else {
+            return nil
+        }
+        return Project(name: name, dueDate: formatAirTableDate(date: datePicker.date) )
+    }
+    
+    private func formatAirTableDate(date: Date) -> String {
+        return date.description.components(separatedBy: .whitespaces)[0]
     }
 }
